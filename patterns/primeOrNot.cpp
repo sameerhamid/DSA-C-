12d9a2322c5e5e39a484df31981e1bb5eff14390
sameer ahmad <sameer@theastrotime.com>
2025-08-12 22:05:29 +0530

    add printColsNumberTriangle

--- patterns/primeOrNot.cpp
+++ patterns/primeOrNot.cpp
@@ -2,21 +2,37 @@
 using namespace std;
 
 
-void printTraingleOfStars (){
-    int rows, cols;
-    cout << "Enter number of rows: " << endl;
-    cin >> rows;
-    cout << "Enter number of columns: " << endl;
-    cin >> cols;
+void printTraingleOfStars(int rows, int cols){
     for(int i = 0; i < rows; i++){
         for(int j = 0; j < cols; j++){
             cout << "* ";
         }
         cout<< endl;
     }
-}
+};
+
+void printColsNumberTraingle(int rows, int cols){
+     for(int i = 1; i <= rows; i++){
+        for(int j = 1; j <= cols; j++){
+            cout << i << " ";
+        }
+        cout<< endl;
+    }
+};
 
 int main(){
-    printTraingleOfStars();
+    int rows, cols;
+    cout << "Enter number of rows: " << endl;
+    cin >> rows;
+    cout << "Enter number of columns: " << endl;
+    cin >> cols;
+
+    printTraingleOfStars(rows,cols);
+
+    cout <<  endl;
+    cout << "-------------- " << endl;
+    cout <<  endl;
+
+    printColsNumberTraingle(rows,cols);
     return 1;
 }
