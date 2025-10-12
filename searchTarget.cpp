ed46421e937faf5ada02fad20937f8ed9a272a29
sameer ahmad <sameer@theastrotime.com>
2025-10-12 17:17:17 +0530

    add problem description

--- searchTarget.cpp
+++ searchTarget.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
+// Problem Description
+// Given a sorted array of N distinct integers and a target value X, return the index if the target is found. If not found then return -1.
+
 int searchTarget(int N,int A[],int X){
       int left = 0;
       int right = N-1;
