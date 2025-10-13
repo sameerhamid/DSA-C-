e1de4878fbcbfd3d7047be0f1495126bde09fc5d
sameer ahmad <sameer@theastrotime.com>
2025-10-13 12:10:30 +0530

    add description for the problem

--- nthFibonaciiNumber.cpp
+++ nthFibonaciiNumber.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
 
-// TODO: Implement this method
+// Problem Description
+// Given an integer n, you have to find the nth fibonacci number. The fibonacci sequence is given by 0,1,1,2,3,5,8,... where 0 and 1 are the 0th and 1st fibonacci numbers respectively and every consecutive number is the sum of the previous two numbers in the sequence.
+
+
 int nthFibonacciNumber(int n){
       if(n==0){
             return 0;
