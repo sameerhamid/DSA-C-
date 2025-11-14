dec03d0abfa726c33e306a2bd6ae44c2819db842
Sameer <sameer@theastrotime.com>
2025-11-14 12:33:58 +0530

    add taylor series

---
+++ recursion/taylorsSeries.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+double taylorSeriesSum(int x, int n){
+    static double p = 1, f = 1;
+    double r;
+    if(n==0){
+        return 1;
+    }
+    r = taylorSeriesSum(x, n-1);
+    p = p * x;
+    f = f * n;
+    return r + p / f;
+
+}
+
+int main(){
+    cout << taylorSeriesSum(3, 10) << endl;
+    return 0;
+}
