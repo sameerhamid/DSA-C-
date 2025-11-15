6af55b5cbe2121663fda7f7c84ce620edf7841d4
Sameer <sameer@theastrotime.com>
2025-11-15 23:24:37 +0530

    implement taylor series using honers rule by both itrative and recursive approach

--- recursion/taylorsSeries.cpp
+++ recursion/taylorsSeries.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 
+// time complexity -> o(n*n)
 double taylorSeriesSum(int x, int n){
     static double p = 1, f = 1;
     double r;
@@ -13,8 +14,33 @@ double taylorSeriesSum(int x, int n){
     return r + p / f;
 
 }
+// Honers Rule
+// now using loop with time complexity -> o(n)
+
+double taylorSeriesSumUsingLoop(int x, int n){
+    double r = 1.0;
+    while(n>0){
+        r = 1 + r * x/n;
+        n--;
+    }
+    return r;
+}
+
+// Honers Rule
+// now using recursion with time complexity -> on(n);
+
+double taylorSeriesSumOptomize(int x, int n){
+    static double r = 1.0;
+    if(n==0){
+        return r;
+    }
+    r = 1 + r * x / n;
+    return taylorSeriesSumOptomize(x, n-1);
+}
 
 int main(){
-    cout << taylorSeriesSum(3, 15) << endl;
+    cout << "taylorSeriesSum: " << taylorSeriesSum(3, 15) << endl;
+    cout << "taylorSeriesSumUsingLoop: " <<  taylorSeriesSumUsingLoop(3, 15) << endl;
+    cout << "taylorSeriesSumOptomize: " << taylorSeriesSumOptomize(3, 15) << endl;
     return 0;
 }
